25a704592149eaeeb7ece16ff81cae8e9cf55f34
banan314 <klopusz492@outlook.com>
2017-04-30 20:02:32 +0200

    add Angular frontend stub

--- src/main/java/hw/spring/model/exception/NoSuchUserException.java
+++
@@ -1,7 +0,0 @@
-package hw.spring.model.exception;
-
-/**
- * Created by Kamil on 01-Apr-17.
- */
-public class NoSuchUserException extends Exception {
-}
--- src/main/java/hw/spring/model/user/Sex.java
+++
@@ -1,3 +0,0 @@
-package hw.spring.model.user;
-
-public enum Sex {MALE, FEMALE}
