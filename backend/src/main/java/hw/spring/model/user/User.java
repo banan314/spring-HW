2fb1b9a36898838ec1f3b8a5644e9c84fc401a53
Kamil Łopuszański <kam.wopush314@gmail.com>
2017-09-27 23:18:15 +0200

    added swagger documentation

--- backend/src/main/java/hw/spring/model/user/User.java
+++ backend/src/main/java/hw/spring/model/user/User.java
@@ -19,8 +19,6 @@ import java.util.List;
 @Table(name = "users")
 public class User implements Serializable, Comparable<User> {
 
-
-
     public enum Role {ADMIN, STUDENT;}
     private @Id @GeneratedValue int id;
 
