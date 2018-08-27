0863dc50b6e316171ec91ab61f7a70d7e9049c87
Kamil Łopuszański <kam.wopush314@gmail.com>
2018-08-27 13:02:30 +0200

    import a test user and roles

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,2 +1,4 @@
--- insert into public."name" ("id", "name") values (1, 'ROLE_STUDENT');
--- insert into public."name" ("id", "name") values (2, 'ROLE_ADMIN');
+insert into public."name" ("id", "name") values (1, 'ROLE_STUDENT');
+insert into public."name" ("id", "name") values (2, 'ROLE_ADMIN');
+insert into public."users" ("id", "name", "username", "password", "email", "date_of_birth", "age")
+  values (30, 'test', 'test', 'test', 'test\@test.com', current_date, 20);
