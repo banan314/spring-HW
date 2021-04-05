472150677da91b49f4d5dd7c092e1a0b43a1441c
Kamil Łopuszański <kam.wopush314@gmail.com>
2021-04-05 15:34:51 +0200

    unused variable

--- backend/src/main/java/hw/spring/common/email/EmailValidator.java
+++ backend/src/main/java/hw/spring/common/email/EmailValidator.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 
 public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
    private Pattern pattern;
-   private Matcher matcher;
    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$";
 
    public void initialize(ValidEmail constraint) {
