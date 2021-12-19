2eee1412f39e2cec038e3d5e78c31e1dbe6ad94b
Kamil Łopuszański <kam.wopush314@gmail.com>
2021-12-19 17:03:51 +0100

    rename a table in SQL

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,5 +1,5 @@
 insert into public."roles" ("id", "name") values (1, 'ROLE_STUDENT');
 insert into public."roles" ("id", "name") values (2, 'ROLE_ADMIN');
 insert into public."users" ("id", "name", "username", "password", "lastpasswordresetdate", "email", "date_of_birth", "age") values (30, 'test', 'test', '$2a$10$11mQXna4JSy.WLF9tLGfsuwCuidS3jQ80Fp.ugPIavultQ9yCIwym', current_date - INTERVAL '1 DAY', 'test@test.com', current_date, 20);
-insert into public."role_users" (users, roles) values (30, 1);
-insert into public."role_users" (users, roles) values (30, 2);
+insert into public."roles_users" (users, roles) values (30, 1);
+insert into public."roles_users" (users, roles) values (30, 2);
